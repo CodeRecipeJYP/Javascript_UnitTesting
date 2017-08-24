ab37dafb2e6c9155b303ae14ebd70c1e854b0a04
pjo901018 <pjo901018@gmail.com>
2017-08-25 00:40:37 +0900

    Greater Expectations / write test with chai

---
+++ textUtilities.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect;
+
+function titleCase(title) {
+    return title;
+}
+
+expect(titleCase('the great mouse detective')).to.be.a('string');
+expect(titleCase('the great mouse detective')).to.equal('The Greate Mouse Detective');
