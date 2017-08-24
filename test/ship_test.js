1b363634775dc7631a14ab41ad11d324e59150db
pjo901018 <pjo901018@gmail.com>
2017-08-25 05:06:08 +0900

    Writing Our First Test Suite Part2 / Making more specs

--- test/ship_test.js
+++ test/ship_test.js
@@ -26,4 +26,38 @@ describe('checkForShip', function () {
 
         expect(checkForShip(player, [0, 0])).to.be.true;
     });
+
+    it('should handle ships located at more than one coordinate', function () {
+        player = {
+            ships: [
+                {
+                    locations: [[0, 0], [0, 1]]
+                }
+            ]
+        };
+
+        // be careful for the expects more than one. It can be hard to read.
+        expect(checkForShip(player, [0, 1])).to.be.true;
+        expect(checkForShip(player, [0, 0])).to.be.true;
+        expect(checkForShip(player, [9, 9])).to.be.false;
+    });
+
+    it('should handle checking multiple ships', function () {
+        player = {
+            ships: [
+                {
+                    locations: [[0, 0], [0, 1]]
+                },
+                {
+                    locations: [[1, 0], [1, 1]]
+                }
+            ]
+        };
+
+        expect(checkForShip(player, [0, 1])).to.be.true;
+        expect(checkForShip(player, [0, 0])).to.be.true;
+        expect(checkForShip(player, [1, 0])).to.be.true;
+        expect(checkForShip(player, [1, 1])).to.be.true;
+        expect(checkForShip(player, [9, 9])).to.be.false;
+    });
 });
